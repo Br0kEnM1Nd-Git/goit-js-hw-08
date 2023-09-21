73ac20ba3edce4cf91f38e92bc75b532c1a22586
Br0kEnM1Nd-Git <mrgamer594@gmail.com>
2023-09-21 12:23:44 +0300

    task 1 end - vimeo init

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,23 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+gallery.innerHTML = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
+    <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" data-source="${original}">
+    </a>
+    </li>`;
+  })
+  .join('');
+
+const simpleGallery = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
